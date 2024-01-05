a3a3d1022fff91505e09db57beee8963c8ca2f3b
Ezekiel Newren <ezekielnewren@gmail.com>
2024-01-04 21:51:35 -0700

    openssl recommend using prime256v1 instead of secp256r1

--- copy/entrypoint.sh
+++ copy/entrypoint.sh
@@ -4,8 +4,8 @@ ADDR=$1
 . /copy/validate.sh
 
 cd /vault
-openssl ecparam -name secp256r1 -genkey -noout -out vault.key
 openssl req -new -nodes -key vault.key -x509 -days 1 -subj "/CN=$FQDN" -out vault.pem
+openssl ecparam -name prime256v1 -genkey -noout -out vault.key
 
 mkdir -p /vault/config
 cat /copy/config.hcl | sed -e "s/ADDR_API/$ADDR/g" -e "s/ADDR_CLUSTER/$FQDN:$(($PORT+1))/g" -e "s/PORT/$PORT/g" > /vault/config/config.hcl
